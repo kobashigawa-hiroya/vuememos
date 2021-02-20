901b7b7796f924e169488f9d355c3da2e88b36a3
hiroya <hiroya19890721@gmail.com>
2021-02-21 07:17:44 +0900

    Style変更

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -47,9 +47,48 @@ export default {
   }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+<style lange="scss" scoped>
+.form {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   margin: 32px;
+   &-group {
+     margin-bottom: 1rem;
+   }
+   &-control {
+     width: 600px;
+     min-height: 24px;
+     font-size: 1rem;
+     border: 1px solid #ced4da;
+     padding: 4px 8px;
+   }
+  }
+
+  button {
+    width: 200px;
+  }
+
+  .flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .card {
+    color:#fff;
+    background-color: black;
+    width: 238px;
+    border: 1px solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+    margin: 16px;
+    &-body {
+      padding: 1.25rem;
+    }
+    &-title {
+      margin-bottom: .75rem;
+      font-weight: 600;
+    }
+  }
 </style>
